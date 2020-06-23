c745c211cb59261c17beb1e83c541a9ee71c2568
Campari229 <KennyKoin@gmail.com>
2020-06-23 15:08:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.9dbe766b973c94641d89d63005472064.js"
 );
 
 self.addEventListener('message', (event) => {
